bc5d98ccb2b49b95086e03f8127d7dc7982efe39
apdlvmfgkrhtlvek <kimsun050820@gmail.com>
2025-07-22 03:26:36 +0900

    md파일 저장 구현 + 코드 함수로 분할

--- src/repo/repo.service.ts
+++ src/repo/repo.service.ts
@@ -1,5 +1,5 @@
 import { CreateRepoDto } from 'src/dto/RequestDto/CreateRepoDto';
-import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import axios from 'axios';
@@ -7,6 +7,7 @@ import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import * as CryptoJS from "crypto-js"
 import { Repo } from 'src/entities/repo.entity';
+import { Post } from 'src/entities/post.entity';
 
 @Injectable()
 export class RepoService {
@@ -16,21 +17,26 @@ export class RepoService {
         
         @InjectRepository(Repo)
         private readonly repoRepository : Repository<Repo>,
+
+        @InjectRepository(Post)
+        private readonly postRepository : Repository<Post>,
         
         private readonly configService : ConfigService,
     ) {}
 
-    async getRepo(userId:number) {
+    private header(token:string) {
+        return {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/vnd.github.v3+json',
+        }
+    }
+
+    async getMdFilesPaths(userId:number):Promise <{repo:string[]}> {
 
         const token = await this.tokenDecrypt(userId)
         const url = `https://api.github.com/user/repos`;
 
-        const repo = await axios.get(url, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/vnd.github.v3+json',
-            }
-        });
+        const repo = await axios.get(url,{headers: this.header(token)});
 
         const repoNames = repo.data.map(r => r.name);
         return {
@@ -42,17 +48,12 @@ export class RepoService {
         const { repoName,ignorePath,refreshIntervalMinutes } = dto
         const token = await this.tokenDecrypt(userId)
 
-        const headers = {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/vnd.github.v3+json',
-        }
-
         const mdFiles:string[] = []
-        
+
         const borwseDir = async (path:string) => {
             const url = `https://api.github.com/repos/${username}/${repoName}/contents/${path}`
          
-            const res = await axios.get(url, {headers});
+            const res = await axios.get(url, {headers:this.header(token)});
 
             const data = res.data
         
@@ -64,36 +65,56 @@ export class RepoService {
                 }
             }
         }   
-
-        const findAllContent = async () => {
-            for(const path of mdFiles) {
-                const url = `https://api.github.com/repos/${username}/${repoName}/contents/${path}`
-                const res = await axios.get(url, {headers});
-                const content = Buffer.from(res.data.content, 'base64').toString('utf-8')
-                console.log(content)
-            }
-        }
-
+    
         await borwseDir('')
 
         const user = await this.userRepository.findOneBy({id:userId})
         if(!user) throw new BadRequestException("토큰 정보가 올바르지 않습니다")
 
-        // try {
-        //     await this.repoRepository.save({
-        //         user,
-        //         repo:mdFiles,
-        //         refresh_interval_minutes:refreshIntervalMinutes,
-        //         ignore_path:ignorePath
-        //     })
-        // } catch(e) {
-        //     throw new BadRequestException("유저 중복 오류")
-        // }
+        try {
+            await this.repoRepository.save({
+                user,
+                repo:mdFiles,
+                refresh_interval_minutes:refreshIntervalMinutes,
+                ignore_path:ignorePath
+            })
+        } catch(e) {
+            throw new BadRequestException()
+        }
 
-        await findAllContent()
+        await this.createPosts(userId,username,repoName,token,mdFiles)
     }
 
-    async tokenDecrypt(userId:number) {
+    async createPosts(
+        userId:number,
+        username:string,
+        repoName:string,
+        token:string,
+        mdFiles:string[]
+    ) {
+        const user = await this.userRepository.findOneBy({id:userId})
+        if(!user) throw new BadRequestException("토큰 정보가 올바르지 않습니다")
+
+        for(const path of mdFiles) {
+            const url = `https://api.github.com/repos/${username}/${repoName}/contents/${path}`
+            const res = await axios.get(url, {headers:this.header(token)});
+            let content = Buffer.from(res.data.content, 'base64').toString('utf-8')
+            let firstLine = content.split('\n')[0]
+            
+            console.log(firstLine)
+            
+            if(firstLine == "") {
+                firstLine ="무제"
+            }
+            await this.postRepository.save({ 
+                user,
+                title:firstLine,
+                content
+            })
+        }
+    }
+
+    async tokenDecrypt(userId:number):Promise <string> {
         const secretKey = this.configService.get<string>("CRYPTO_SECRET")
 
         const res = await this.userRepository.findOneBy({ id:userId })
